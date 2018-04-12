8179147b058f66aa5f1d5322a2ac6fbe789f24a2
Douglas Campos <qmx@qmx.me>
2018-04-12 12:36:43 +0000

    jenkins

---
+++ jenkins/ssh_key.groovy
@@ -0,0 +1,23 @@
+#!groovy
+
+import jenkins.model.*
+import com.cloudbees.plugins.credentials.*
+import com.cloudbees.plugins.credentials.common.*
+import com.cloudbees.plugins.credentials.domains.*
+import com.cloudbees.plugins.credentials.impl.*
+import com.cloudbees.jenkins.plugins.sshcredentials.impl.*
+
+global_domain = Domain.global()
+	credentials_store =
+	Jenkins.instance.getExtensionList(
+			'com.cloudbees.plugins.credentials.SystemCredentialsProvider'
+			)[0].getStore()
+	credentials = new BasicSSHUserPrivateKey(
+			CredentialsScope.GLOBAL,
+			"jenkins-hyrule",
+			"jenkins",
+			new BasicSSHUserPrivateKey.FileOnMasterPrivateKeySource("/mnt/secrets/jenkins.sshkey"),
+			"",
+			""
+			)
+credentials_store.addCredentials(global_domain, credentials)
