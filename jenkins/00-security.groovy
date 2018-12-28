8dc2d559a9e75b93b72990568b8580a99b9e54b8
Douglas Campos <qmx@qmx.me>
2018-12-28 04:22:50 +0000

    change script ordering

--- jenkins/00-security.groovy
+++ jenkins/00-security.groovy
@@ -1,5 +1,6 @@
 #!groovy
 
+import jenkins.model.*
 import hudson.security.SecurityRealm
 import org.jenkinsci.plugins.GithubSecurityRealm
 import org.jenkinsci.plugins.GithubAuthorizationStrategy
@@ -9,7 +10,6 @@ def github_client_id = System.getenv('GITHUB_CLIENT_ID')
 def github_client_secret = System.getenv('GITHUB_CLIENT_SECRET')
 def github_oauth_scopes = System.getenv('GITHUB_OAUTH_SCOPES') ?: GithubSecurityRealm.DEFAULT_OAUTH_SCOPES
 
-
 if (!Jenkins.instance.isQuietingDown()) {
 	if (github_client_id && github_client_secret && github_admin_user) {
 		def realm = new GithubSecurityRealm(GithubSecurityRealm.DEFAULT_WEB_URI, GithubSecurityRealm.DEFAULT_API_URI, github_client_id, github_client_secret, github_oauth_scopes)
@@ -17,7 +17,7 @@ if (!Jenkins.instance.isQuietingDown()) {
 			Jenkins.instance.setSecurityRealm(realm)
 			Jenkins.instance.save()
 		}
-		def strategy = new GithubAuthorizationStrategy(github_admin_user, false, false, false, null, true, false, false, false)
+		def strategy = new GithubAuthorizationStrategy(github_admin_user, false, false, false, "", true, false, false, false)
 		if (!strategy.equals(Jenkins.instance.getAuthorizationStrategy())) {
 			Jenkins.instance.setAuthorizationStrategy(strategy)
 			Jenkins.instance.save()
