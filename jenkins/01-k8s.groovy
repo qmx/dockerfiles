634368e7f97fb39ebb0b2caf13b21a5dfbfbd82e
Douglas Campos <qmx@qmx.me>
2018-12-28 19:53:13 +0000

    last try

--- jenkins/01-k8s.groovy
+++ jenkins/01-k8s.groovy
@@ -3,47 +3,15 @@
 import jenkins.model.*
 import org.csanchez.jenkins.plugins.kubernetes.KubernetesCloud
 import org.csanchez.jenkins.plugins.kubernetes.PodTemplate
+import org.csanchez.jenkins.plugins.kubernetes.ContainerTemplate
 
 if (!Jenkins.instance.isQuietingDown()) {
 	def kubernetes = new KubernetesCloud("kubernetes")
+	def slaveContainer = new ContainerTemplate("jenkins-slave", "jenkinsci/jnlp-slave:latest")
+	slaveContainer.setTtyEnabled(true)
 	def podTemplate = new PodTemplate()
 	podTemplate.setName("jenkins-slave")
-	podTemplate.setYaml("""
-apiVersion: v1
-kind: Pod
-spec:
-  containers:
-  - name: jnlp
-    image: jenkinsci/jnlp-slave:latest
-    command: ['cat']
-    tty: true
-    volumeMounts:
-    - name: dockersock
-      mountPath: /var/run/docker.sock
-    - name: jenkins-keys
-      readOnly: true
-      mountPath: "/mnt/secrets"
-  - name: docker
-    image: docker:18.09.0
-    command: ['cat']
-    tty: true
-    volumeMounts:
-    - name: dockersock
-      mountPath: /var/run/docker.sock
-    - name: jenkins-keys
-      readOnly: true
-      mountPath: "/mnt/secrets"
-  volumes:
-  - name: dockersock
-    hostPath:
-      path: /var/run/docker.sock
-  - name: jenkins-keys
-    secret:
-      secretName: jenkins
-      items:
-        - key: "jenkins.key"
-          path: "jenkins.key"
-""")
+	podTemplate.getContainers().add(slaveContainer)
 	kubernetes.addTemplate(podTemplate)
 	Jenkins.instance.clouds.removeAll(KubernetesCloud)
 	Jenkins.instance.clouds.add(kubernetes)
