6631739ae848a8c8f3f10b6b5e0ef17ef359799c
Douglas Campos <qmx@qmx.me>
2018-12-29 04:13:00 +0000

    more

--- jenkins/01-k8s.groovy
+++ jenkins/01-k8s.groovy
@@ -4,11 +4,14 @@ import jenkins.model.*
 import org.csanchez.jenkins.plugins.kubernetes.KubernetesCloud
 import org.csanchez.jenkins.plugins.kubernetes.PodTemplate
 import org.csanchez.jenkins.plugins.kubernetes.ContainerTemplate
+import org.csanchez.jenkins.plugins.kubernetes.volumes.workspace.EmptyDirWorkspaceVolume
 
 if (!Jenkins.instance.isQuietingDown()) {
 	def kubernetes = new KubernetesCloud("kubernetes")
 	def slaveContainer = new ContainerTemplate("jenkins-slave", "jenkinsci/jnlp-slave:latest")
 	slaveContainer.setTtyEnabled(true)
+	slaveContainer.setCommand("/bin/sh -c")
+	slaveContainer.setArgs("cat")
 	def podTemplate = new PodTemplate()
 	podTemplate.setName("jenkins-slave")
 	podTemplate.setNamespace("default")
@@ -17,7 +20,9 @@ if (!Jenkins.instance.isQuietingDown()) {
 	podTemplate.setNodeSelector("")
 	podTemplate.setNodeUsageMode(hudson.model.Node.Mode.NORMAL)
 	podTemplate.setCustomWorkspaceVolumeEnabled(false)
+	podTemplate.setWorkspaceVolume(new EmptyDirWorkspaceVolume(false))
 	podTemplate.getContainers().add(slaveContainer)
+	podTemplate.setYaml("")
 	kubernetes.setDefaultsProviderTemplate("")
 	kubernetes.addTemplate(podTemplate)
 	kubernetes.setServerUrl("")
